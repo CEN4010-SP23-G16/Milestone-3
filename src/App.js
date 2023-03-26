94b315267e07da1ab66c262b9d9d240e180fdb62
Jehad392 <116598965+Jehad392@users.noreply.github.com>
2023-03-26 14:44:00 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Header from './0_Components/Header';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import HomePage from './HomePage/HomePage';
+import WeatherPage from './WeatherPage/WeatherPage';
+import MapsRadarPage from './MapsRadarPage/MapsRadarPage';
+import AlertPage from './AlertPage/AlertPage';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/weather' element={<WeatherPage/>}/>
+        <Route path='/maps-radar' element={<MapsRadarPage/>}/>
+        <Route path='/alerts' element={ <AlertPage/>} />
+      </Routes>
+    </>
+  );
+  
+}
+
+export default App;
