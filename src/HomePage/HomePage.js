8466e9fcaf3c6eb40c2cbb18466032a40394bb51
Jehad392 <116598965+Jehad392@users.noreply.github.com>
2023-03-27 14:48:08 -0400

    Add files via upload

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,11 +4,13 @@ import './home.css';
 import { TfiLocationArrow } from "react-icons/tfi";
 import { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
 
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
+  const navigate = useNavigate();
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ea2ebba4214c311f36ba1cc2619ed14b`
 
@@ -17,16 +19,14 @@ export default function HomePage() {
       const location = event.target.value;
       axios.get(url).then((response) => {
         setData(response.data)
-        console.log(response.data)
-        const queryString = `location=${location}`;
-        window.location.href = `/WeatherPage${queryString}`;
+        console.log('Data from API:', response.data);
+        navigate(`/weather?location=${location}`, { state: response.data });
       })
       setLocation('')
     }
   }
 
   return (
-
     <>
       <div className='HomePage'></div>
 
@@ -50,65 +50,4 @@ export default function HomePage() {
       </div>
     </>
   )
-}import React from 'react';
-import { Container } from 'react-bootstrap';
-import SearchBar from '../0_Components/SearchBar';
-import './home.css';
-import { TfiLocationArrow } from "react-icons/tfi";
-import { useState } from 'react';
-import axios from 'axios'
-
-export default function HomePage() {
-
-    function App() {
-        const [data,setData] = useState({})
-        const [location, setLocation] = useState('')
-        
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=${location}&appid={ea2ebba4214c311f36ba1cc2619ed14b}"
-
-        const searchLocation = (event) => {
-            if (event.key === "Enter") {
-
-                axios.get(url).then((response) => {
-                    setData(response.data)
-                    console.log(response.data)
-                })
-                setLocation('')
-            }
-        }
-        return (
-
-            <>
-                <div className='background'>
-                    <Container>
-
-                        {/* For the image */}
-                        <div className='logo'>
-
-                        </div>
-                        <div>
-
-
-                        </div>
-
-                        <div className='search'>
-                                <input
-                                    value={location} type={text} onchange={event => setLocation(event.target.value)}
-                                    placeholder='Enter location' className='fontstyle' onKeyPress={searchLocation}>
-                            
-                                </input>
-                                <TfiLocationArrow className='icon' />
-
-                            <p>
-                                ClimateSmart is a beautiful and simple weather application that displays the weather information, gives travel and clothing recommendations, personalized recommendations weather alerts, and more.
-                            </p>
-
-                        </div>
-
-                    </Container>
-                </div>
-            </>
-
-        )
-    }
 }
--- src/WeatherPage/WeatherPage.js
+++ src/WeatherPage/WeatherPage.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import LocationSearch from '../0_Components/LocationSearch';
 
-export default function WeatherPage() {
+export default function WeatherPage(props) {
+  const state = props.location?.state;
+  console.log('state:', state); // Add this line to see if the state is defined
+  const { weatherData } = state || {};
+  console.log('weatherData:', weatherData); // Add this line to see if weatherData is defined
+
   return (
     <>
       <Container>
-        <LocationSearch/>
+        <LocationSearch />
+        <div>
+          {weatherData ? (
+            <>
+              <h1>{weatherData.name}, {weatherData.sys.country}</h1>
+              <h2>{weatherData.weather[0].main}</h2>
+              <p>Temperature: {weatherData.main.temp}°F</p>
+              <p>Humidity: {weatherData.main.humidity}%</p>
+              <p>Wind Speed: {weatherData.wind.speed} mph</p>
+            </>
+          ) : (
+            <h1>No weather data found</h1>
+          )}
+        </div>
       </Container>
-      
     </>
-  )
+  );
 }
